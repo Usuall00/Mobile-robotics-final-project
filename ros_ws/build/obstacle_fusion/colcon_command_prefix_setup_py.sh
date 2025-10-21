d40496aa44e44254790129c844277d9bf88c4a2b
Usuall00 <simonesolito00@gmail.com>
2025-10-21 18:45:40 +0200

    Versione (21/10)

--- ros_ws/build/obstacle_fusion/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/ros_workspace/install/dynamic_obstacle_detector_interfaces/share/dynamic_obstacle_detector_interfaces/package.sh"
